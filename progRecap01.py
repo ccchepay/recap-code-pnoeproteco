d563e11ef87b33ed1f1177a7467f075ea1ff42f9
Pedro Noe Hernandez Gutierrez <pedronoe@comunidad.unam.mx>
2024-02-17 12:31:55 -0600

    Creation

---
+++ progRecap01.py
@@ -0,0 +1,44 @@
+##arrays declaration
+array = [1,2,3,4,5,6,7,8,9,10]
+qualities=["alto","bronceado","guapo","rico","sexy","tierno","rudo"]
+
+#hello world
+print("hello world")
+
+##for loop and if
+for i in range(1,10):
+    if(i%2!=0):
+        print(f"{i} no es par")
+
+##for loop?
+# for i in array:
+#     if(i%2!=0):
+#         print(f"{i} no es par")
+
+#python and switch and read options
+# for quality in qualities:
+#     prompt=input(f"Es {quality}? (Y/N)")
+#     if(prompt=='N'):
+#         print("no te conviene")
+#         break
+# if(prompt =='Y'):
+#     print("Apurese o se lo ganan")
+
+##append to array
+# if(input("Deseas agregar más condiciones?(Y/N)")=="Y"):
+#     qualities.append(input("Escribe la propiedad: "))
+
+##print array
+# print("Quealities:")
+# print (qualities)
+
+# ## While
+# flag = True
+# while(flag):
+#     if(input("Deseas agregar más condiciones?(Y/N)")=="Y"):
+#         qualities.append(input("Escribe la propiedad: "))
+#     else:
+#         flag=False
+# print("Quealities:")
+# print (qualities)
+
